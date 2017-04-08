0fc72542dce5fa5fdf2c6a085270745575df00e8
RussellLuo <luopeng.he@gmail.com>
2017-04-08 15:07:02 +0800

    Improve the example worker based on Redis

--- examples/redis/main.go
+++ examples/redis/main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	addr := flag.String("addr", ":6379", "the TCP address of Redis")
 	queue := flag.String("queue", "", "the task queue name in Redis")
-	concurrency := flag.Int("concurrency", 1, "the number of goroutines (each has a connection to Redis) to spawn for task handling")
+	concurrency := flag.Int("concurrency", 1, "the number of goroutines to spawn for task handling")
+	connections := flag.Int("connections", 1, "the maximum number of Redis connections")
 	intervalStr := flag.String("interval", "10ms", "the interval for polling the task queue")
 
 	flag.Parse()
@@ -36,6 +37,7 @@ func main() {
 			Addr:        *addr,
 			Queue:       *queue,
 			Concurrency: *concurrency,
+			Connections: *connections,
 			Interval:    interval,
 		},
 	}
--- examples/redis/worker.go
+++ examples/redis/worker.go
@@ -10,13 +10,14 @@ import (
 	"time"
 
 	"github.com/RussellLuo/gotask"
-	"github.com/garyburd/redigo/redis"
+	"github.com/go-redis/redis"
 )
 
 type Options struct {
 	Addr        string
 	Queue       string
 	Concurrency int
+	Connections int
 	Interval    time.Duration
 }
 
@@ -26,21 +27,18 @@ type RedisWorker struct {
 }
 
 func (w *RedisWorker) Work() error {
-	conns := []redis.Conn{}
-	for i := 0; i < w.Opts.Concurrency; i++ {
-		conn, err := redis.Dial("tcp", w.Opts.Addr)
-		if err != nil {
-			return err
-		}
-		defer conn.Close()
-		conns = append(conns, conn)
-	}
+	client := redis.NewClient(&redis.Options{
+		Addr:        w.Opts.Addr,
+		DB:          0, // use default DB
+		PoolSize:    w.Opts.Connections,
+		PoolTimeout: 10 * time.Second, // wait 10s for connection
+	})
 
 	var wg sync.WaitGroup
-	for _, conn := range conns {
+	for i := 0; i < w.Opts.Concurrency; i++ {
 		wg.Add(1)
 
-		go func(conn redis.Conn) {
+		go func() {
 			sigChan := make(chan os.Signal, 1)
 			signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
@@ -50,34 +48,33 @@ func (w *RedisWorker) Work() error {
 					wg.Done()
 					return
 				default:
-					err := w.handle(conn)
+					err := w.handle(client)
 					if err != nil {
 						log.Printf("err: %#v", err)
 					}
 					time.Sleep(w.Opts.Interval)
 				}
 			}
-		}(conn)
+		}()
 	}
 
 	wg.Wait()
 	return nil
 }
 
-func (w *RedisWorker) handle(conn redis.Conn) error {
-	bytes, err := conn.Do("LPOP", w.Opts.Queue)
-	if err != nil {
+func (w *RedisWorker) handle(client *redis.Client) error {
+	taskSig, err := client.LPop(w.Opts.Queue).Result()
+	if err == redis.Nil {
+		// Got no task
+		return nil
+	} else if err != nil {
 		return err
 	}
 
-	// Got no task
-	if bytes == nil {
-		return nil
-	}
-	log.Printf("Got task: %s", bytes)
+	log.Printf("Got task: %s", taskSig)
 
 	sig := gotask.Signature{}
-	if err := json.Unmarshal(bytes.([]byte), &sig); err != nil {
+	if err := json.Unmarshal([]byte(taskSig), &sig); err != nil {
 		return err
 	}
 
